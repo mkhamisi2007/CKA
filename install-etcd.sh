498b18f93cc0e24d652639f5d35651534a1a02c6
mkhamisi2007 <31432794+mkhamisi2007@users.noreply.github.com>
2025-11-08 11:27:28 +0100

    Update install-etcd.sh

--- install-etcd.sh
+++ install-etcd.sh
@@ -2,7 +2,7 @@ wget https://github.com/etcd-io/etcd/releases/download/v3.5.15/etcd-v3.5.15-linu
 tar xvf etcd-v3.5.15-linux-amd64.tar.gz
 sudo mv etcd-v3.5.15-linux-amd64/etcd* /usr/local/bin/
 etcd --version
-etcd
+etcd --data-dir /var/lib/etcd-restored
 ----------------------
 export ETCDCTL_API=3
 etcdctl put mykey "Hello etcd"
@@ -13,4 +13,5 @@ etcdctl get user --prefix
 ----
 etcdctl snapshot save backup.db
 etcdutl snapshot status backup.db
+sudo pkill etcd 2>/dev/null || true
 etcdutl snapshot restore backup.db --data-dir /var/lib/etcd-restored
