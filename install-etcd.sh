228a2bbe23cba97aa0c2308934b1325c43ff2cce
mkhamisi2007 <31432794+mkhamisi2007@users.noreply.github.com>
2025-11-08 11:01:13 +0100

    Enhance install-etcd.sh with etcdctl commands
    Added etcdctl commands for key-value operations and snapshot management.

---
+++ install-etcd.sh
@@ -0,0 +1,16 @@
+wget https://github.com/etcd-io/etcd/releases/download/v3.5.15/etcd-v3.5.15-linux-amd64.tar.gz
+tar xvf etcd-v3.5.15-linux-amd64.tar.gz
+sudo mv etcd-v3.5.15-linux-amd64/etcd* /usr/local/bin/
+etcd --version
+etcd
+----------------------
+export ETCDCTL_API=3
+etcdctl put mykey "Hello etcd"
+etcdctl get mykey
+etcdctl del mykey
+etcdctl get "" --prefix --keys-only
+etcdctl get user --prefix
+----
+etcdctl snapshot save backup.db
+etcdutl snapshot status backup.db
+etcdctl snapshot restore backup.db --data-dir /var/lib/etcd-restored
