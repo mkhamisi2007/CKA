864a8d5da317fa9b57548786f4b0a487652a33f8
mkhamisi2007 <31432794+mkhamisi2007@users.noreply.github.com>
2025-12-22 06:58:12 +0100

    Update Linux-commands.sh

--- Networking/Linux-commands.sh
+++ Networking/Linux-commands.sh
@@ -1,3 +1,4 @@
+--------------------------------------General----------------------------------
 # نمایش اینترفیس‌های شبکه
 ip link
 
@@ -18,12 +19,18 @@ ip route add 192.168.1.0/24 via 192.168.2.1
 
 # بررسی فعال بودن IP Forwarding
 cat /proc/sys/net/ipv4/ip_forward
-
+---------------------------------------DNS-----------------------------------
 # local DNS (first priority)
 echo "192.168.1.115 db" >> /etc/hosts
+
 # Set DNS Server (second priority)
 echo "nameserver 192.168.1.120" >> /etc/resolv.conf  #---> local DNS server
 echo "nameserver 8.8.8.8" >> /etc/resolv.conf  #---> internet DNS server
+
 # we can change priority in /etc/nsswitch.conf
 1) hosts:  files dns 
 2) hosts:  dns files 
+
+# search in /etc/resolv.conf
+echo "search dev.example.com example.com" >> /etc/resolv.conf
+ping api #---> ping api.dev.example.com , ping api.example.com
