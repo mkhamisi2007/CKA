c04f550b3ee9d42c971e71c14da1b90378a2ec7a
mkhamisi2007 <31432794+mkhamisi2007@users.noreply.github.com>
2025-12-22 07:33:07 +0100

    Update Linux-commands.sh

--- Networking/Linux-commands.sh
+++ Networking/Linux-commands.sh
@@ -40,6 +40,47 @@ options edns0 trust-ad
 # edns0 ===> Use modern DNS
 # trust-ad ===> trust the response if it is verified by DNSSEC
 
+# give more information 
+nslookup www.google.com
+    Server:         127.0.0.53
+    Address:        127.0.0.53#53
+    
+    Non-authoritative answer:
+    Name:   www.google.com
+    Address: 142.250.179.100
+    Name:   www.google.com
+    Address: 2a00:1450:4007:818::2004
+dig www.google.com
+    ; <<>> DiG 9.18.39-0ubuntu0.24.04.2-Ubuntu <<>> www.google.com
+    ;; global options: +cmd
+    ;; Got answer:
+    ;; ->>HEADER<<- opcode: QUERY, status: NOERROR, id: 44812
+    ;; flags: qr rd ra; QUERY: 1, ANSWER: 1, AUTHORITY: 0, ADDITIONAL: 1
+    
+    ;; OPT PSEUDOSECTION:
+    ; EDNS: version: 0, flags:; udp: 65494
+    ;; QUESTION SECTION:
+    ;www.google.com.                        IN      A
+    
+    ;; ANSWER SECTION:
+    www.google.com.         213     IN      A       142.250.179.100
+    
+    ;; Query time: 1 msec
+    ;; SERVER: 127.0.0.53#53(127.0.0.53) (UDP)
+    ;; WHEN: Mon Dec 22 07:11:45 CET 2025
+    ;; MSG SIZE  rcvd: 59
+
+---------------------------------CoreDNS------------------------------------------------------------
+curl -LO https://github.com/coredns/coredns/releases/download/v1.12.4/coredns_1.12.4_linux_amd64.tgz
+$ tar -zxf coredns_1.12.4_linux_amd64.tgz 
+---------------------------------CoreDNS in Kubernetes----------------------------------------------
+kubectl describe configmap coredns -n kube-system
+
+# Service name: backend
+# Namespace: default
+# backend.default.svc.cluster.local
+curl backend #---> in pod we just call service name
+
 
 
 
