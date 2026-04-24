feb5de4c7eb9370e9e8d8e2a5fb0e7a734fdfb4f
mkhamisi2007 <31432794+mkhamisi2007@users.noreply.github.com>
2026-04-24 09:54:37 +0200

    Update command.sh

--- Admission-Controller/example/command.sh
+++ Admission-Controller/example/command.sh
@@ -1,3 +1,4 @@
 kubectl create ns admission-demo
+kubectl create secret docker-registry my-docker-secret --docker-username=mkhamisi2007 --docker-password=XXXX --docker-email=m_khamisi_2007@yahoo.com
 docker build -t mkhamisi2007/admission-python:v1 .
 docker push mkhamisi2007/admission-python:v1
